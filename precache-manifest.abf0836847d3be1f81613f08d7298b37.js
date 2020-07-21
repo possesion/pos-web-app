22befa43f8ff3cce900fda82d3c7b5c5705adb77
pos <possesion193@gmail.com>
2020-07-21 10:41:56 +0300

    Updates

---
+++ precache-manifest.abf0836847d3be1f81613f08d7298b37.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95895dee6a0048558f88d95786469fe",
+    "url": "/pos-web-app/index.html"
+  },
+  {
+    "revision": "e7c3fadc968f87ff4987",
+    "url": "/pos-web-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e706e918561343aa7dc6",
+    "url": "/pos-web-app/static/css/main.c670c484.chunk.css"
+  },
+  {
+    "revision": "e7c3fadc968f87ff4987",
+    "url": "/pos-web-app/static/js/2.4a0f0a59.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/pos-web-app/static/js/2.4a0f0a59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e706e918561343aa7dc6",
+    "url": "/pos-web-app/static/js/main.3e048da9.chunk.js"
+  },
+  {
+    "revision": "ed4fd4dddabd8b830868",
+    "url": "/pos-web-app/static/js/runtime-main.efca6490.js"
+  },
+  {
+    "revision": "2588565563f620be3be6c8091800e5a7",
+    "url": "/pos-web-app/static/media/bg.25885655.jpg"
+  },
+  {
+    "revision": "8ceae62e02a48772aacadd66075cce30",
+    "url": "/pos-web-app/static/media/game.8ceae62e.png"
+  },
+  {
+    "revision": "5b7cb12a39aa81d7e6c1de0ec7554a42",
+    "url": "/pos-web-app/static/media/logo2.5b7cb12a.svg"
+  },
+  {
+    "revision": "9f93805a8d996aa5dde981bff825a386",
+    "url": "/pos-web-app/static/media/portfolioS.9f93805a.jpg"
+  },
+  {
+    "revision": "cb95ba0e8bf6e7b155088bf4070f63d3",
+    "url": "/pos-web-app/static/media/profile2.cb95ba0e.jpg"
+  }
+]);
